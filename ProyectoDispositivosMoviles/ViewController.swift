016c78fdb843c7cc2f7febea77d9c370c957e346
Allan Iván Ramírez Alanís <a01169661@imac-de-macmaster.local>
2017-02-13 14:21:47 -0600

    Initial Commit

---
+++ ProyectoDispositivosMoviles/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProyectoDispositivosMoviles
+//
+//  Created by Allan Iván Ramírez Alanís on 13/02/17.
+//  Copyright © 2017 Allan Iván Ramírez Alanís. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
